27009480d1c27a9db51026b575a367ecb6f944e7
Generally1 <54377185+Generally1@users.noreply.github.com>
2021-09-09 02:47:40 -0400

    Create ddoslog.sh

---
+++ ddoslog.sh
@@ -0,0 +1,15 @@
+interface=eth0
+ dumpdir=/root/tcpdumpsbackup2
+ while /bin/true; do
+ pkt_old=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
+ sleep 1
+ pkt_new=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
+ pkt=$(($pkt_new-$pkt_old))
+ echo -ne "\r$pkt packets/s\033[0K"
+ if [ $pkt -gt 2000 ]; then
+ echo -e "\n`date` Under attack, dumping packets." 
+tcpdump -n -s0 -c 2000 -w $dumpdir/dump.`date +"%Y%m%d-%H%M%S"`.cap
+echo "`date` Packets dumped, sleeping now."
+sleep 60
+fi
+done
